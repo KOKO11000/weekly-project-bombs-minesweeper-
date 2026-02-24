5705b8344f9b0c1176a7b0b5618ffb3c08274c04
netanel <netaneloz1998@gmail.com>
2026-02-24 11:53:44 +0200

    create h1 in main to create conflict

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ function App() {
         <h1 className="text-4xl font-bold text-gray-800 mb-4">
           Welcome to Vite + React + Tailwind
         </h1>
+        <h1>create h1 on main to create conflict with two branchs</h1>
         <p className="text-gray-600">
           Start building your amazing app!
         </p>
